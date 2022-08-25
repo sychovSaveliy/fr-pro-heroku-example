bd9084306d7d0457da57fda298d93c84ffc147df
Savelii Sychov <savelii@miro.com>
2022-08-25 20:04:22 +0200

    server start

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const port = 8080;
 
 app.use(express.static(path.resolve(__dirname, "./build")));
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
